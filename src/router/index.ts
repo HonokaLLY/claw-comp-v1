f8c267fef9205fbaa92cef89bfdb24026d37bc51
Your Name <linyili_martin@outlook.com>
2026-03-18 18:06:36 +0800

    fix: redirect with base path

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: '/community'
+      redirect: import.meta.env.BASE_URL + 'community'
     },
     {
       path: '/community',
